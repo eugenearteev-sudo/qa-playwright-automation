d86ee4db024133b2240c9caef10c570cb52518c8
eugenearteev-sudo <eugene.arteev@gmail.com>
2026-03-12 06:01:36 +0300

    Add negative API test

--- tests/api-httpbin.spec.js
+++ tests/api-httpbin.spec.js
@@ -26,3 +26,47 @@ test('API: POST login simulation', async ({ request }) => {
     expect(body.json.password).toBe('SuperSecretPassword!');
 
 });
+
+// 
+test('API: response has required headers and body structure', async ({ request }) => {
+    const response = await request.post('https://httpbin.org/post', {
+        data: {
+            username: 'alex',
+            password: '123456',
+        }
+    });
+
+    expect(response.status()).toBe(200);
+    expect(response.headers()['content-type']).toContain('application/json');
+
+    const body = await response.json();
+
+    expect(body.json).toBeDefined();
+    expect(typeof body.json.username).toBe('string');
+    expect(typeof body.json.password).toBe('string');
+
+    expect(body.json.username).not.toBe('');
+    expect(body.json.password).not.toBe('');
+});
+
+// Проверка negative input: проверяем, что сервер не падает, API отвечает корректно, структура ответа не ломается
+
+test('API: negative request with empty body', async ({ request }) => {
+    const response = await request.post('https://httpbin.org/post', {
+        data: {}
+    });
+
+    // Статус ответа
+    expect(response.status()).toBe(200);
+
+    // headers
+    expect(response.headers()['content-type']).toContain('application/json');
+
+    const body = await response.json();
+
+    // Структура ответа
+    expect(body.json).toBeDefined();
+
+    // Тело запроса пустое
+    expect(body.json).toEqual({});
+});
