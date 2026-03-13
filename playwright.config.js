8c451310b39c990e46aa4cd072ede20a748c3498
eugenearteev-sudo <eugene.arteev@gmail.com>
2026-03-13 06:23:35 +0300

    Add HTML reporter

--- playwright.config.js
+++ playwright.config.js
@@ -6,6 +6,8 @@ module.exports = defineConfig({
   // чтобы trace появился: нужен retry
   retries: 1,
 
+  reporter: [['html']],
+
   use: {
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
